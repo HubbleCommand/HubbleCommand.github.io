411e5c08c1f37ad2e8ac9a50db3a8e55599e3110
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2025-04-05 19:45:58 +0200

    repo stars add counts to headings

--- assets/js/repo_stars.js
+++ assets/js/repo_stars.js
@@ -16,9 +16,14 @@ function call(url, onData) {
 
 function fetchHubbleRepoStars() {
 	call("https://api.github.com/users/HubbleCommand/repos", data => {
+		var totalStars = 0
 		var starred = data.filter(element => {
+			totalStars += element.stargazers_count
 			return element.stargazers_count > 0;
 		});
+
+		document.getElementById("my-repos-that-have-been-starred").append(` (x ${totalStars})`)
+
 		var sorted = starred = starred.sort((a, b) => {
 			return b.stargazers_count - a.stargazers_count
 		})
@@ -36,6 +41,8 @@ function fetchHubbleRepoStars() {
 
 function fetchHubbleStarredRepos() {
 	call("https://api.github.com/users/HubbleCommand/starred", data => {
+		document.getElementById("repos-ive-starred").append(` (x ${data.length})`)
+
 		var grid = document.getElementById("starred-repos")
 		data.forEach(repo => {
 			grid.innerHTML += `<div style="display: inline-block; padding: 20px; text-align: center;"><a href="${repo.html_url}">${repo.name}</a></div>`
